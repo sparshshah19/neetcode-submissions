c9e929d304008b07313d6ac93954fca583051a04
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-23 23:13:47 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #so how binary search works is you want to compute the middle, 
+        left = 0 
+        right = len(nums) - 1
+
+        while left <= right: 
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+        
+        return -1 
+
+
