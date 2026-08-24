55064e1f36fff4d6a21abbd7610b5472b4d77c0d
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-24 14:41:37 -0400

    Update: is-anagram - submission-4

--- Data Structures & Algorithms/is-anagram/submission-4.py
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -1,23 +1,5 @@
-from collections import defaultdict
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        #using sorting 
 
-        count_s, count_t = defaultdict(int), defaultdict(int)
-
-        for i in range(len(s)):
-            count_s[s[i]] += 1
-            count_t[t[i]] += 1
-
-        for key in count_s.keys():
-            if count_s[key] != count_t[key]:
-                return False
-
-        return True
-
-        
-
-
-
+       return sorted(s) == sorted(t)
