6fe20deef70011fffc5eec061b3c9147b6deb30d
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-24 13:41:16 -0400

    Update: three-integer-sum - submission-2

--- Data Structures & Algorithms/three-integer-sum/submission-2.py
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -1,30 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        #first we can sort it   
+        ans_ls = []
         nums.sort()
 
-        for i, val in enumerate(nums):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                three_sum = val + nums[l] + nums[r]
-                if three_sum > 0:
-                    r -= 1
-                elif three_sum < 0:
-                    l += 1
-                elif three_sum == 0:
-                    res.append([val, nums[l], nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-        
-        return res
+        for i, n in enumerate(nums): 
+            if n > 0: 
+                break 
+            if i > 0 and n == nums[i - 1]:
+                continue 
+            
+            left = i + 1                                
+            right = len(nums) - 1
 
-
-
-
-
-        
-
-        
+            while left < right: 
+                three_sum = n + nums[left] + nums [right]
+                if three_sum > 0: 
+                    right -= 1
+                elif three_sum < 0: 
+                    left += 1 
+                else: 
+                    ans_ls.append([n, nums[left], nums[right]])
+                    left += 1 
+                    right -= 1 
+                    while left < right and nums[left] == nums[left - 1]: 
+                        left += 1
+        return ans_ls
+    
