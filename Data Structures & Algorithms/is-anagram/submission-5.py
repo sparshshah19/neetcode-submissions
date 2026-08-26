8dd2c3d413324b3f22e646c3a4b3f5c97142be64
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-26 14:54:33 -0400

    Update: is-anagram - submission-5

--- Data Structures & Algorithms/is-anagram/submission-5.py
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -1,22 +1,5 @@
-from collections import defaultdict
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        #using sorting 
 
-        hash_map = defaultdict(int)
-
-        for c in s:
-            hash_map[c] += 1
-
-        for c in t:
-            hash_map[c] -= 1
-
-        for value in hash_map.values():
-            if value != 0:
-                return False
-
-        return True
-            
-        
+        return sorted(s) == sorted(t)
