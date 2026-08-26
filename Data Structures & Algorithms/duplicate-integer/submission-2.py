5be3f202505cba67523b0c065bdccd9f1707ef85
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-26 14:50:10 -0400

    Update: duplicate-integer - submission-2

--- Data Structures & Algorithms/duplicate-integer/submission-2.py
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -1,20 +1,10 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        # Initialize the set
-        my_set = set()
+        seen = set()
 
-        # For num in nums
-        for num in nums:
-
-            # If num is found in my_set
-            if (num in my_set):
-
-                # We are done we can return True
-                return True
-
-            # Otherwise add the num to the set
-            my_set.add(num)
-        
-        # If loop finishes return False
+        for num in nums: 
+            if num in seen: 
+                return True 
+            else: 
+                seen.add(num)
         return False
-         
