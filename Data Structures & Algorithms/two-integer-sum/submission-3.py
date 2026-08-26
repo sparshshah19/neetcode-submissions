e0cb01dc04216fe148b3551cb558e6bd1f8ed644
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-26 15:10:56 -0400

    Update: two-integer-sum - submission-3

--- Data Structures & Algorithms/two-integer-sum/submission-3.py
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -1,15 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
 
-        hash_table = {}
+        for i, n in enumerate(nums): 
+            needed = target - n
+            if needed in num_map: 
+                return [num_map[needed], i]
+            else: 
+                num_map[n] = i
 
-        for i, j in enumerate(nums):
-            diff = target - nums[i]
-
-            if diff in hash_table.keys():
-                return [hash_table[diff], i]
-
-            hash_table[j] = i
-
-        
-        
+        #0:3 #needed = 4 num_map = 3:0
+        #1:4 #needed = 3 
+        #2:5 #needed = 2
+        #3:6 #needed = 1          
