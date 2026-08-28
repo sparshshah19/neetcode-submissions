cdef2dbea4914f934869acc7525487148807f3de
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-28 10:22:36 -0400

    Update: is-anagram - submission-8

--- Data Structures & Algorithms/is-anagram/submission-8.py
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -1,21 +1,4 @@
-from collections import defaultdict
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-       
-        freq_s = defaultdict(int)
-        freq_t = defaultdict(int)
-
-        for c in s:
-            freq_s[c] += 1
-        for c in t:
-            freq_t[c] += 1
-
-        for key, value in freq_s.items():
-            if freq_s[key] != freq_t[key]:
-                return False
-
-        return True
-        
+        #during anagram questions you can use hashmaps
+        return sorted(s) == sorted(t)
