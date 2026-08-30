628079d4742355c297d89079a774ac0426abe240
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-30 16:31:49 -0400

    Update: products-of-array-discluding-self - submission-4

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
@@ -1,14 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)
+        ##brute force 
+    #
+    #   return_arr = []
 
+   ##      for j in range(len(nums)):
+      #          if i == j: 
+      #              continue 
+     #           ans *= nums[j]
+      ##      return_arr.append(ans)
+     #   return return_arr
+    
+        #so basically 
+        #result product = prefix product * suffix product 
+        #prefix product is product of all the elements to the right 
+        #suffix product is product of all the elements to the left of i 
+        #store in two seperate elements of len(nums)
+        #and then multiply them going in reverse or su
+        res_arr = [1] * (len(nums))
         prefix = 1
         for i in range(len(nums)):
-            res[i] = prefix
+            res_arr[i] = prefix
             prefix *= nums[i]
-        postfix = 1
+        
+        postfix = 1 
         for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
+            res_arr[i] *= postfix 
             postfix *= nums[i]
+        return res_arr
+        
 
-        return res
