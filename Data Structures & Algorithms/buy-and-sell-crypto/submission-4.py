50fb242110a711ecb8426c5fcae808413edef2c5
Sparsh Shah <sparsh.shah@uwaterloo.ca>
2026-08-31 16:13:01 -0400

    Update: buy-and-sell-crypto - submission-4

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -1,18 +1,29 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
+    def maxProfit(self, prices: List[int]) -> int: 
+        #we want to basically buy low, sell high 
         max_profit = 0
 
-        while len(prices) > r:
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
-            else:
-                l = r
-            r += 1
+        #slidingwindowquestion 
 
-        return max_profit
+        #brute force solution 
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                profit = prices[j] - prices[i]
+                max_profit = max(profit, max_profit)
 
+        return max_profit 
 
+        #slidingwindow
 
+        max_profit = 0 
+        left = 0 
+        right = 1
+        while right < len(prices):
+            if prices[right] > prices[left]:
+                profit = prices[right] - prices[left]
+                max_profit = max(profit, max_profit)
+            else: 
+                left = right 
+            right += 1
         
+        return max_profit
